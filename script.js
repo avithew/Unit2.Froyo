32c6f60a074e9eb2d481e4e26183a585f6b140e2
avithew <avithew@gmail.com>
2024-08-09 21:16:46 -0600

    created functions getFlavors (prompt) and getFlavorTable (returns object)

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-let flavors = prompt("enter ur flavors").split(",");
-console.log(flavors);
+const flavors = getFlavors();
+const flavorTable = getFlavorTable(flavors);
+console.log(flavorTable);
+
+//returns string array of flavors provided by user
+function getFlavors() {
+  return prompt("enter ur flavors").split(",");
+}
+
+//takes a string array of flavors
+//returns an object listing flavors w/ corresponding amounts
+function getFlavorTable(flavors) {
+  const flavorTable = {};
+  for (i = 0; i < flavors.length; ++i) {
+    const key = flavors[i];
+    if (key in flavorTable) {
+      flavorTable[key] += 1;
+    } else {
+      flavorTable[key] = 1;
+    }
+  }
+  return flavorTable;
+}
+
+function printFlavorTable() {}
